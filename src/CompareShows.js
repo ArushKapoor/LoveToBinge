f502428d1a631136f3e8fad20cc349c22a9878c9
ArushKapoor <arush0113.cse19@chitkara.edu.in>
2021-06-24 09:41:42 +0530

    added ELO Rating System

--- src/CompareShows.js
+++ src/CompareShows.js
@@ -16,8 +16,6 @@ function CompareShows() {
   useEffect(() => {
     console.log("These are posts", posts[0]);
 
-    console.log(Math.random());
-
     db.collection("anime")
       .doc(posts[0])
       .get()
@@ -53,6 +51,82 @@ function CompareShows() {
 
   const showName = (name) => {
     console.log("Clicked", name);
+
+    let ratingFirst, ratingSecond;
+
+    db.collection("anime")
+      .doc(firstShow?.name)
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          // console.log("Document data:", doc.data().url);
+          // setFirstShow(doc.data());
+          // console.log("This is data ", doc.data().rating);
+          ratingFirst = doc.data().rating;
+          db.collection("anime")
+            .doc(secondShow?.name)
+            .get()
+            .then((doc) => {
+              if (doc.exists) {
+                // console.log("Document data:", doc.data().url);
+                // setFirstShow(doc.data());
+                // console.log("This is data ", doc.data().rating);
+                ratingSecond = doc.data().rating;
+
+                // ELO Rating
+                let expectedRatingFirst =
+                  1.0 /
+                  (1.0 + Math.pow(10, (ratingSecond - ratingFirst) / 400));
+                expectedRatingFirst = +(
+                  Math.round(expectedRatingFirst + "e+2") + "e-2"
+                );
+
+                let expectedRatingSecond =
+                  1.0 /
+                  (1.0 + Math.pow(10, (ratingFirst - ratingSecond) / 400));
+                expectedRatingSecond = +(
+                  Math.round(expectedRatingSecond + "e+2") + "e-2"
+                );
+
+                let scoreFirst, scoreSecond;
+                if (firstShow?.name == name) {
+                  scoreFirst = 1;
+                  scoreSecond = 0;
+                } else {
+                  scoreFirst = 0;
+                  scoreSecond = 1;
+                }
+
+                let newRatingFirst =
+                  ratingFirst + 32 * (scoreFirst - expectedRatingFirst);
+                let newRatingSecond =
+                  ratingSecond + 32 * (scoreSecond - expectedRatingSecond);
+
+                newRatingFirst = +(Math.round(newRatingFirst + "e+2") + "e-2");
+
+                newRatingSecond = +(
+                  Math.round(newRatingSecond + "e+2") + "e-2"
+                );
+
+                console.log(
+                  `newRatingFirst: ${newRatingFirst}, newRatingSecond: ${newRatingSecond}`
+                );
+              } else {
+                // doc.data() will be undefined in this case
+                console.log("No such document!");
+              }
+            })
+            .catch((error) => {
+              console.log("Error getting document:", error);
+            });
+        } else {
+          // doc.data() will be undefined in this case
+          console.log("No such document!");
+        }
+      })
+      .catch((error) => {
+        console.log("Error getting document:", error);
+      });
   };
 
   return (
