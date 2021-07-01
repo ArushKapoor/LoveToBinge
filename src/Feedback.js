ef95201a4f3e009aec0a886b9e234626d62685eb
ArushKapoor <arush0113.cse19@chitkara.edu.in>
2021-07-01 22:41:51 +0530

    added styling to feedback form

--- src/Feedback.js
+++ src/Feedback.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { db } from "./firebase.js";
 import "./Feedback.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 function Feedback() {
   const [name, setName] = useState("");
@@ -20,36 +21,33 @@ function Feedback() {
 
   return (
     <div className="feedback">
-      <h2>Any other show or changes you would like to see?</h2>
-      <form className="feedback__form">
-        <div className="feedback__name">
-          <input
-            value={name}
-            onChange={(event) => setName(event.target.value)}
-            placeholder={"Name"}
-          />
-          <span style={{ color: "grey", fontSize: "13px", marginLeft: "5px" }}>
-            *Optional
-          </span>
-        </div>
-        <div className="feedback__feedback">
-          <textarea
-            value={feedback}
-            onChange={(event) => setFeedback(event.target.value)}
-            placeholder={"Your feedback..."}
-          />
-          <span style={{ color: "red", fontSize: "13px", marginLeft: "5px" }}>
-            *Required
-          </span>
-        </div>
-        <button
-          className="feedback__submit"
-          onClick={sendFeedback}
-          type="submit"
-        >
-          Submit
-        </button>
-      </form>
+      <div className="feedback__container container">
+        <p className="feedback__question font-weight-bold">
+          Any other show or changes you would like to see?
+        </p>
+        <form className="feedback__form">
+          <div className="feedback__name">
+            <input
+              value={name}
+              onChange={(event) => setName(event.target.value)}
+              placeholder={"Name"}
+            />
+          </div>
+          <div className="feedback__feedback">
+            <textarea
+              value={feedback}
+              onChange={(event) => setFeedback(event.target.value)}
+              placeholder={"Your feedback..."}
+            />
+            <span>*</span>
+          </div>
+          <div className="feedback__submit">
+            <button onClick={sendFeedback} type="submit">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
