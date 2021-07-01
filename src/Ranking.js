a5386e5cbf812308db38b968d7c04ad7710bb826
ArushKapoor <arush0113.cse19@chitkara.edu.in>
2021-07-01 19:23:40 +0530

    fixing carousel

--- src/Ranking.js
+++ src/Ranking.js
@@ -4,10 +4,12 @@ import { useStateValue } from "./StateProvider";
 import { db } from "./firebase.js";
 import RankShow from "./RankShow";
 import Filters from "./Filters";
+import { Carousel } from "react-bootstrap";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 function Ranking() {
   const [shows, setShows] = useState([]);
+  const [topShows, setTopShows] = useState([]);
 
   const [{ posts, filter }] = useStateValue();
 
@@ -23,8 +25,14 @@ function Ranking() {
         entries.sort(function (a, b) {
           return b.rating - a.rating;
         });
+        var temp = [];
         setShows(entries);
+        for (var i = 0; i < 2; i++) {
+          temp.push(entries[i]);
+        }
+        setTopShows(temp);
         console.log("Entries: ", entries);
+        // console.log("Top Entries: ", topShows);
       })
       .catch((error) => {
         console.log("Error getting documents: ", error);
@@ -34,6 +42,30 @@ function Ranking() {
   return (
     <div className="ranking">
       <div className="ranking__container container">
+        {shows.length != 0 && (
+          <Carousel
+            className="ranking__carousel"
+            nextLabel={""}
+            prevLabel={""}
+            interval={4000}
+            prevIcon={""}
+            nextIcon={""}
+            slide={true}
+            wrap={true}
+            ref={React.createRef()}
+          >
+            {topShows?.map((show, index) => (
+              <Carousel.Item>
+                <img
+                  className="carousel__img"
+                  src={show?.url}
+                  alt={show?.name}
+                  key={index}
+                />
+              </Carousel.Item>
+            ))}
+          </Carousel>
+        )}
         <Filters />
         {shows.map((show, index) => (
           <RankShow rank={index + 1} name={show?.name} img={show?.url} />
