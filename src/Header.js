93b488651aed3c7ed07b43448232e77b248cb65f
ArushKapoor <arush0113.cse19@chitkara.edu.in>
2021-07-02 19:39:27 +0530

    changed header.js

--- src/Header.js
+++ src/Header.js
@@ -3,11 +3,9 @@ import "./Header.css";
 
 function Header() {
   return (
-    <div className="header">
-      <nav className="header__top">
-        <h1>Building Love To Binge</h1>
-      </nav>
-    </div>
+    <nav className="header">
+      <p className="header__name">Love To Binge</p>
+    </nav>
   );
 }
 
