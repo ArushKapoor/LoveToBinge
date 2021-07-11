8426e1399c9779a8dcce51109f32b19ef50a090d
ArushKapoor <arush0113.cse19@chitkara.edu.in>
2021-07-11 16:58:25 +0530

    added title and logo

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,15 @@
 import React from "react";
 import "./Header.css";
 import { Link } from "react-router-dom";
+import title from "./images/title.png";
 
 function Header() {
   return (
     // Creating the navbar or the header of website
     <nav className="header">
       <Link className="header__link" to="/">
-        <p className="header__name">Love To Binge</p>
+        {/* <p className="header__name">Love To Binge</p> */}
+        <img className="header__title" src={title} alt="Title" />
       </Link>
     </nav>
   );
